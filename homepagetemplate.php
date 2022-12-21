12af698849a93eb47efde7951beb303cf0674f02
LAKXHANA SELVA RAJAH <38977549+lakxhana@users.noreply.github.com>
2022-12-21 22:34:45 +0800

    Lakxhana (#4)
    * commited website template
    
    * css template for website template ("style1")
    
    Co-authored-by: Lakxhana <lakxhanaselvarajah.gmail.com>

---
+++ homepagetemplate.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   </head>
+<body>
+  <nav>
+    <div class="menu">
+      <div class="logo">
+        <a href="#">Apovlita.</a>
+      </div>
+      <ul>
+        <li class="nav-item"><a class="nav-link active" href="homepage.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Profile</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Receipts</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Help Center</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Log Out</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="background"></div>
+  <div class="center">
+    <div class="title">Apovlita</div>
+  </div>
+</body>
+</html>
