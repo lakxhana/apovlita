d23e53afef837f9eed13f02755ae54011717c578
IqramRafie <iqram.mohdrafie@gmail.com>
2023-01-04 06:23:16 +0800

    not much

--- createRequest.php
+++ createRequest.php
@@ -16,14 +16,25 @@ if(isset($_POST['createRequest']))
 	$serial = $_POST['itemSerialNum'];
 	$unit = $_POST['itemUnit'];
 
-	var_dump($type);
+	// var_dump($type);
 	foreach($serial as $item)
 	{
-		// var_dump($serial);
+		$search = "SELECT * FROM item WHERE 'p_serialNum' = ".$serial[$item]."";
+		$sresult = mysqli_query($conn, $search);
+
+		if(mysqli_num_rows($sresult) > 0)
+		{
+			
+		}
+		else
+		{
+			$query =  "INSERT INTO item ('p_type', 'p_model', 'p_brand', 'p_serialNum', 'p_unit') VALUES (".$type[$item].", ".$model[$item].", ".$brand[$item].", ".$serial[$item].", ".$unit[$item].")";
+			$result =  mysqli_query($conn, $query);
+		}
+		// var_dump($item);
 		// echo $item;
 	}
-
-	$query =  "INSERT INTO item ('p_type', 'p_model', 'p_brand', 'p_serialNum', 'p_unit') VALUES ()";
+	
 }
 
 
