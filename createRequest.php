f7b984bdc13b70510d9424bc36aeb9fe883a3c35
IqramRafie <iqram.mohdrafie@gmail.com>
2023-01-06 00:04:05 +0800

    nothing much

--- createRequest.php
+++ createRequest.php
@@ -15,10 +15,36 @@ if(isset($_POST['createRequest']))
 	$brand = $_POST['itemBrand'];
 	$serial = $_POST['itemSerialNum'];
 	$unit = $_POST['itemUnit'];
+	$reqName = $_POST['requestName'];
+	$reqDetails = $_POST['requestDetails'];
+
+
+
+	$collector = "SELECT * FROM collector ORDER BY RAND() LIMIT 1 ";
+	$resultC = mysqli_query($conn, $collector);
+	// var_dump($resultC);
+	$collector;
+	$cust;
+
+	while($row = mysqli_fetch_assoc($resultC))
+	{
+		$yaoyao = "SELECT * FROM customer WHERE userId = ".$_SESSION['id']."";
+		$resyaoyao = mysqli_query($conn, $yaoyao);
+		$rowyaoyao = mysqli_fetch_assoc($resyaoyao);
+
+		$query2 = "INSERT INTO request (custId, collectorId, requestName, requestDetails, requestDate, status) VALUES ('".$rowyaoyao['custId']."', '".$row['collectorId']."', '$reqName', '$reqDetails', '".date("Y/M/D")."', 'requesting')";
+		$result2 = mysqli_query($conn, $query2);
+		// var_dump($query2);
+		// echo $result2;
+		$collector = $row['collectorId'];
+		$cust = $rowyaoyao['custId'];
+	}
 
 	// var_dump($type);
-	foreach($serial as $item)
+	foreach($serial as $item => $key)
 	{
+		// var_dump($item);
+		// echo $key;
 		$search = "SELECT * FROM item WHERE 'p_serialNum' = ".$serial[$item]."";
 		$sresult = mysqli_query($conn, $search);
 
@@ -28,15 +54,29 @@ if(isset($_POST['createRequest']))
 		}
 		else
 		{
-			$query =  "INSERT INTO item (p_type, p_model, p_brand, p_serialNum, p_unit) VALUES (".$type[$item].", ".$model[$item].", ".$brand[$item].", ".$serial[$item].", ".$unit[$item].")";
+			$query =  "INSERT INTO item (p_type, p_model, p_brand, p_serialNum, p_unit) VALUES ('".$type[$item]."', '".$model[$item]."', '".$brand[$item]."', '".$serial[$item]."', '".$unit[$item]."')";
 			$result =  mysqli_query($conn, $query);
+			var_dump($result);
 
-			$query2 = "INSERT INTO request () VALUES ()";
+			$nahida = "SELECT * FROM request WHERE custId = '$cust' AND collectorId = ".$collector."";
+			$resnahida = mysqli_query($conn, $nahida);
+			// var_dump($resnahida);
+			$rownahida = mysqli_fetch_assoc($resnahida);
 
+			$qiqi = "SELECT * FROM item WHERE p_type = '".$type[$item]."' AND p_model = '".$model[$item]."' AND p_brand = '".$brand[$item]."' AND p_serialNum = '".$serial[$item]."' AND p_unit = '".$unit[$item]."' ";
+			$resqiqi = mysqli_query($conn, $qiqi);
+			// var_dump($qiqi);
+			$rowqiqi = mysqli_fetch_assoc($resqiqi);
+
+			$klee = "INSERT INTO item_list (reqId, itemId) VALUES (".$rownahida['reqId'].", ".$rowqiqi['itemId'].")";
+			$resklee = mysqli_query($conn, $klee);
+			var_dump($klee);
 		}
 		// var_dump($item);
 		// echo $item;
 	}
+
+	header("Location:currentRequest.php");
 	
 }
 
